bbd0d030c5cfbdb767cecaaef88e47c646df8b8c
Карастелева Наталья Николаевна <Nnkarasteleva@ac.atlantconsult.com>
2021-02-18 18:28:02 +0300

    feat: create brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var stack = [];
+  for (let index = 0; index < str.length; index++) {
+    stack.push(str[index]);
+    if(stack.length > 1){
+      for (let key in bracketsConfig) {
+        if(stack[stack.length - 1] == bracketsConfig[key][1] && stack[stack.length - 2] == bracketsConfig[key][0]){
+          stack.pop();
+          stack.pop();
+        }
+      }
+    }
+  }
+  if(stack.length == 0){
+    return true;
+  }else{
+    return false;
+  }
 }
